cbadb54c3315627a7b2db0ca013d7b41f35ec0d2
bekzat serikbayev <serikbay_b@qwasar.io>
2022-05-19 15:24:58 +0000

    tetriiiiiiis

--- src/game.js
+++ src/game.js
@@ -27,6 +27,9 @@ export default class Game {
     activePiece = {
         x: 0,
         y: 0,
+        get blocks() {
+            return this.rotations[this.rotationIndex];
+        },
         blocks: [
             [0,1,0],
             [1,1,1],
@@ -56,6 +59,39 @@ export default class Game {
         if (this.hasCollision()){
             this.activePiece.y -= 1;
             this.lockPiece();
+        }
+    }
+
+    rotatePiece() {
+        this.rotateBlocks();
+        if (this.hasCollision()) {
+            this.rotateBlocks(false);
+        }
+        
+    }
+
+    rotateBlocks(clockwise = true) {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = i; j < y - i; j++) {
+                const temp = blocks[i][j];
+
+                if (clockwise) {
+                    blocks[i][j] = blocks[y - j][i];
+                    blocks[y - j][i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[j][y - i]
+                    blocks[j][y - i] = temp;
+                }else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }  
+            }
         }
     }
 
