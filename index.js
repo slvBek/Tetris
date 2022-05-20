cdbf3e8eed4bada8af37534a416aacca975060ff
bekzat serikbayev <serikbay_b@qwasar.io>
2022-05-20 15:10:53 +0000

    bekzaaaaaat

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+    switch (event.keyCode) {
+        case 37:
+            game.movePieceLeft();
+            view.render(game.getState());
+            break;
+        case 38:
+            game.rotatePiece();
+            view.render(game.getState());
+            break;
+        case 39:
+            game.movePieceRight();   
+            view.render(game.getState());
+            break;
+        case 40:
+            game.movePieceDown();
+            view.render(game.getState());
+            break;
+    }    
+});
+
+
