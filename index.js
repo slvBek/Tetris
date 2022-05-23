4c09818132fb6afc3a8121e1ad99cd9770e44a6a
bekzat serikbayev <serikbay_b@qwasar.io>
2022-05-23 15:05:35 +0000

    That was not easy

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 import Game from './src/game.js';
 import View from './src/view.js';
+import Controller from './src/controller.js';
 
 const root = document.querySelector('#root');
 
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.game = game;
 window.view = view;
-
-document.addEventListener('keydown', event => {
-    switch (event.keyCode) {
-        case 37:
-            game.movePieceLeft();
-            view.render(game.getState());
-            break;
-        case 38:
-            game.rotatePiece();
-            view.render(game.getState());
-            break;
-        case 39:
-            game.movePieceRight();   
-            view.render(game.getState());
-            break;
-        case 40:
-            game.movePieceDown();
-            view.render(game.getState());
-            break;
-    }    
-});
+window.controller = controller;
